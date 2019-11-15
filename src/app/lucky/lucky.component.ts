16157482cbe800855e051aef74a11401b02d578b
Lê Hồng Quyền <hongquyen196@gmail.com>
2019-11-16 02:23:49 +0700

    add code lucky layout

---
+++ src/app/lucky/lucky.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+declare const odometer: any;
+@Component({
+  selector: 'app-lucky',
+  templateUrl: './lucky.component.html',
+  styleUrls: ['./lucky.component.css']
+})
+export class LuckyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+    setTimeout(() => {
+      odometer.innerHTML = this.randomNumbers();
+    }, 1000);
+  }
+
+  randomNumbers = () => {
+    var val = Math.floor(Math.random() * 999);
+    return ('000' + val).slice(-3);
+  };
+
+}
+
